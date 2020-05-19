e13e9a26342b3ad6e778b9e112d382abcf3c55ca
SeiichiN <billie175@gmail.com>
2020-05-19 16:39:40 +0900

    applyを使う理由

--- congestion.lisp
+++ congestion.lisp
@@ -13,16 +13,26 @@
 ;; *cop-odds* -- 検問 それぞれの道路に15分の1の確率
 (defparameter *cop-odds* 15)
 
+;; 1..30の数値を返す
 (defun random-node ()
   (1+ (random *node-num*)))
 
+;; (a . b)(b . a) のリストを作る
 (defun edge-pair (a b)
   (unless (eql a b)
     (list (cons a b) (cons b a))))
 
+;; applyを使うと、全部をひっくるめた大きなリストになる
+;; ((28 . 6) (6 . 28) (24 . 16) (16 . 24) ...)
 (defun make-edge-list ()
   (apply #'append (loop repeat *edge-num*
                        collect (edge-pair (random-node)(random-node)))))
 
+;; これだと、(((28 . 6)(6 . 28)) ((24 . 16)(16 . 24)) ...) というリス
+;; トになってしまう
+;; (defun make-edge-list ()
+;;   (append (loop repeat *edge-num*
+;;              collect (edge-pair (random-node)(random-node)))))
 
-;;; 修正時刻： Tue May 19 12:31:49 2020
+
+;;; 修正時刻： Tue May 19 16:38:11 2020
