8da67b60925da7b39df8b9cd049e8bb04113c9ff
SeiichiN <billie175@gmail.com>
2020-05-28 13:24:31 +0900

    orc-battle start.

---
+++ orcBattle.lisp
@@ -0,0 +1,30 @@
+;; orcBattle.lisp
+
+(defparameter *monsters* nil)
+(defparameter *monster-builders* nil)
+(defparameter *monster-num* nil)
+
+(defun orc-battle ()
+  (init-monsters)
+  (init-player)
+  (game-loop)
+  (when (player-dead)
+    (princ "You have been killed. Game Over."))
+  (when (monsters-dead)
+    (princ "Congraturations! You have vanquished all of your foes.")))
+
+(defun game-loop ()
+  (unless (or (player-dead) (monsters-dead))
+    (show-player)
+    (dotimes (k (1+ (truncate (/ (max 0 *player-agility*) 15))))
+      (unless (monsters-dead)
+        (show-monsters)
+        (player-attack)))
+    (fresh-line)
+    (map 'list
+         (lambda (m)
+           (or (monster-dead m) (monster-attack m)))
+         *monsters*)
+    (game-loop)))
+
+;; 修正時刻： Thu May 28 13:20:19 2020
