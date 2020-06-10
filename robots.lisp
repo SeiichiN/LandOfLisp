6eb5f06883234c12754f4cfbd56608b5bf1ff821
SeiichiN <billie175@gmail.com>
2020-06-10 20:53:51 +0900

    position of player and position of monster

--- robots.lisp
+++ robots.lisp
@@ -1,5 +1,11 @@
 ;; robots.lisp
 ;;
+;; 盤面は 64 x 16
+;; pos = 544 は 盤面のちょうど真ん中
+;; @ -- player
+;; A -- monster(robots)
+;; # -- scrap
+;;
 (defun robots ()
   (loop named main
      with directions = '((q . -65) (w . -64) (e . -63) (a . -1)
@@ -16,12 +22,21 @@
      for monsters = (loop repeat 10
                        collect (random 1024))
      then (loop for mpos in monsters
+             ;; (count mpos monsters) --- リストmonsters の中の mpos
+             ;;   と等しい要素の個数
+             ;; monsters の中に mpos が複数あれば
              collect (if (> (count mpos monsters) 1)
                          mpos
                          (cdar (sort (loop for (k . d) in directions
+                                        ;; monsters も player と同じ
+                                        ;; 方向に動く
                                         for new-mpos = (+ mpos d)
+                                        ;; monsterの位置とplayerの位置のX座標の差と
+                                        ;; y座標の差の和
                                         collect (cons (+ (abs (- (mod new-mpos 64)
                                                                  (mod pos 64)))
+                                                         ;; (ash n -6) -- 64分の1
+                                                         ;; つまり、64がいくつあるかがわかる
                                                          (abs (- (ash new-mpos -6)
                                                                  (ash pos -6))))
                                                       new-mpos))
@@ -43,4 +58,12 @@
                                  (t
                                   #\ ))))))
 
-;; 修正時刻： Wed Jun 10 04:57:53 2020
+
+(defparameter *directions* '((q . -65) (w . -64) (e . -63) (a . -1)
+                               (d .   1) (z .  63) (x .  64) (c . 65)))
+
+(defparameter *monsters* (loop repeat 10 collect (random 1024)))
+
+
+
+;; 修正時刻： Wed Jun 10 20:53:11 2020
